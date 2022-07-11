3f2237d70e0cc0840ac7d76cbaf0cde6b9cd7280
im_zephyrus <97819866+franciskendrick@users.noreply.github.com>
2022-07-11 18:38:46 +0800

    Added scoreboard module

---
+++ src/scoreboard.py
@@ -0,0 +1,12 @@
+from number_font import NumberFont
+import pygame
+
+pygame.init()
+
+
+class Scoreboard(NumberFont):
+    def __init__(self):
+        super().__init__()
+
+    def draw(self, display):
+        pass
