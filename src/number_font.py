e1ab285041e64246e4242334c588b4a3ccbfd2c3
im_zephyrus <97819866+franciskendrick@users.noreply.github.com>
2022-07-11 18:38:33 +0800

    Added number font's initialize and draw functions

--- src/number_font.py
+++ src/number_font.py
@@ -1,3 +1,4 @@
+from utils import clip_font_to_dict
 from windows import window
 import pygame
 import os
@@ -13,7 +14,46 @@ resources_path = os.path.abspath(
 
 class NumberFont:
     def __init__(self):
-        pass
+        # Get font spriteset
+        font_set = pygame.image.load(
+            f"{resources_path}/number_font.png")
+        font_set.convert()
 
-    def render_font(self, display, text, pos, color=window.white):
-        pass
+        # Order of characters in font spriteset
+        self.order = [
+            "0", "1", "2", "3", "4",
+            "5", "6", "7", "8", "9"
+        ]
+
+        # Get characters dictionary
+        self.characters = clip_font_to_dict(
+            font_set, self.order)
+
+        # Spacing
+        self.character_spacing = 1
+
+    def render_font(self, display, text, pos, enlarge=1):
+        display_handle = pygame.Surface(
+            display.get_size(), pygame.SRCALPHA)
+        x, y = pos
+        x_offset = 0
+
+        # Loop over every character in text
+        for char in text:
+            # Get character image
+            character = self.characters[char]
+
+            # Resize character image
+            wd, ht = character.get_size()
+            resized_character = pygame.transform.scale(
+                character, (wd * enlarge, ht * enlarge))
+
+            # Blit to Handle Display
+            display_handle.blit(
+                resized_character, (x + x_offset, y))
+
+            # Add to offset the width of resized character and spacing
+            x_offset += resized_character.get_width() + self.character_spacing
+        
+        # Blit to screen
+        display.blit(display_handle, (0, 0))
