1ddb95e076dcfffd41ff103b23bab159b0e95aea
im_zephyrus <97819866+franciskendrick@users.noreply.github.com>
2022-07-13 17:49:46 +0800

    Added menu's title module

--- src/windows/menu/menu.py
+++ src/windows/menu/menu.py
@@ -1,4 +1,5 @@
 from windows import window
+from .title import Title
 import pygame
 
 pygame.init()
@@ -9,6 +10,8 @@ class Menu:
         wd, ht = window.rect.size
         self.display = pygame.Surface(
             (wd // 2, ht // 2), pygame.SRCALPHA)
+
+        self.title = Title()
     
     def draw(self, display):
         pass
---
+++ src/windows/menu/title.py
@@ -0,0 +1,11 @@
+import pygame
+
+pygame.init()
+
+
+class Title:
+    def __init__(self):
+        pass
+
+    def draw(self, display):
+        pass
