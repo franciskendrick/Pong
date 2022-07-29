df91a4e17dc15316963c6fda1e54c6cab7f72f69
im_zephyrus <97819866+franciskendrick@users.noreply.github.com>
2022-07-29 14:40:26 +0800

    Added gamemode's buttons' init & draw function
    - Added gamemode's buttons' initialization
    - Added gamemode's buttons' draw function
    - Added gamemode's buttons' spriteset
    - Update gamemode's JSON

--- src/windows/before_game/gamemode/buttons.py
+++ src/windows/before_game/gamemode/buttons.py
@@ -1,3 +1,4 @@
+from utils import clip_set_to_list_on_yaxis, palette_swap
 from windows import window
 import pygame
 import json
@@ -20,11 +21,46 @@ with open(f"{resources_path}/gamemode.json") as json_file:
 class Buttons:
     # Initialize -------------------------------------------------- #
     def __init__(self, display_size_divider):
-        pass
+        spriteset = pygame.image.load(
+            f"{resources_path}/buttons.png")
+        order = ["singleplayer", "multiplayer"]
+        images = clip_set_to_list_on_yaxis(spriteset)
+        enlarge = display_size_divider * window.enlarge
+
+        # Palette
+        hover_palette = {
+            (0, 0, 0): (64, 64, 64),
+            (255, 255, 255): (128, 128, 128)}
+        
+        # Buttons
+        self.buttons = {}
+        for name, img in zip(order, images):
+            # Initialize button
+            hover_img = palette_swap(img.convert(), hover_palette)
+            rect = pygame.Rect(
+                gamemode_data["buttons_positions"][name],
+                img.get_rect().size)
+            hitbox = pygame.Rect(
+                rect.x * enlarge, rect.y * enlarge,
+                rect.width * enlarge, rect.height * enlarge)
+            
+            # Append to buttons
+            button = [
+                False,  # if mouse is over
+                img,  # original image
+                hover_img,  # hover image
+                rect,  # image's rectangle
+                hitbox  # hitbox
+            ]
+            self.buttons[name] = button
 
     # Draw -------------------------------------------------------- #
     def draw(self, display):
-        pass
+        for button in self.buttons.values():
+            mouse_is_over, orig_img, hover_img, rect, _ = button
+            img = hover_img if mouse_is_over else orig_img
+
+            display.blit(img, rect)
 
     # Action detection -------------------------------------------- #
     def button_down_detection(self):
